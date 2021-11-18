edc0bdc057a4e8c4d4245c21fb80a4fb47dcc00c
mozert1988 <mozer-t@hotmail.com>
2021-11-17 22:32:15 -0300

    atualização da rota

--- router/pratosPrincipais.routes.js
+++ router/pratosPrincipais.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const PratoPrincipalController = require('./../controllers/pratoPrincipais.controller');
+const PratoPrincipalController = require('./../controllers/pratosPrincipais.controller');
 
 router.get("/", PratoPrincipalController.index);
 
